57c40dd245e45171dbcda3b2377833ec1bb0cd6b
Sigeno <kobayashi@kobayashinoMacBook-Pro.local>
2018-11-12 00:22:05 +0900

    sample完成!

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p=hfo_bl31smd+bis!37nl+(40zpe51u8hkhe-hub8o6!=y)**'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+
 
 #sample
